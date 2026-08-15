35a37d14af43696337db95d67b6edd0db312ee6f
Muhammad Harit <muhammad.harit@gmail.com>
2026-08-15 22:16:29 +0700

    tambah visi-misi

--- visi-misi.php
+++ visi-misi.php
@@ -1,10 +1,10 @@
-<?php 
-    // Variabel konfigurasi halaman (Dapat diambil dari DB di masa depan)
-    $page_title = "Visi & Misi";
-    $current_page = "profil"; 
+<?php
+// Variabel konfigurasi halaman (Dapat diambil dari DB di masa depan)
+$page_title = "Visi & Misi";
+$current_page = "profil";
 
-    // Include Header
-    include 'includes/header.php'; 
+// Include Header
+include 'includes/header.php';
 ?>
 
 <!-- BANNER HALAMAN -->
@@ -27,7 +27,8 @@
         <div class="vision-card">
             <span class="vision-badge"><i data-lucide="eye"></i> Visi Sekolah</span>
             <p class="vision-text">
-                "Terwujudnya Lulusan yang Bertaqwa, Berkarakter Unggul, Berprestasi Akademik dan Non-Akademik, Berwawasan Lingkungan, serta Mampu Bersaing di Era Global."
+                Unggul dalam prestasi, berkarakter, berbudaya, peduli lingkungan, berwawasan global yang dilandasi iman
+                dan takwa"
             </p>
         </div>
 
@@ -40,23 +41,70 @@
             <div class="misi-card">
                 <div class="misi-number">01</div>
                 <div class="misi-content">
-                    <h4>Pendidikan Karakter & Religius</h4>
-                    <p>Menumbuhkembangkan penghayatan dan pengamalan nilai-nilai keagamaan serta budi pekerti luhur dalam kehidupan sehari-hari.</p>
+                    <h4></h4>
+                    <p>Unggul Membina Peserta didik unggul dalam prestasi akademis dan non akademis di taraf nasional
+                        maupun internasional.</p>
                 </div>
             </div>
             <div class="misi-card">
                 <div class="misi-number">02</div>
                 <div class="misi-content">
-                    <h4>Kualitas Pembelajaran Modern</h4>
-                    <p>Menyelenggarakan proses pembelajaran yang inovatif, berbasis teknologi informasi, dan berpusat pada peserta didik.</p>
+                    <h4></h4>
+                    <p>Membina peserta didik unggul dalam perolehan ujian sekolah dan ujian nasional serta berhasil
+                        masuk perguruan tinggi di dalam maupun di luar negeri.</p>
                 </div>
             </div>
-            <!-- Misi Lainnya... -->
+            <div class="misi-card">
+                <div class="misi-number">03</div>
+                <div class="misi-content">
+                    <h4></h4>
+                    <p>Membudayakan disiplin, toleransi, saling menghargai, percaya diri sehingga terbentuk sikap sikap
+                        peserta didik yang santun dan berbudi pekerti luhur.</p>
+                </div>
+            </div>
+            <div class="misi-card">
+                <div class="misi-number">04</div>
+                <div class="misi-content">
+                    <h4></h4>
+                    <p>Mengembangkan semangat kebangsaan yang berakar pada nilai nilai budaya bangsa dengan tetap
+                        mengikuti perkembangan ilmu pengetahuan dan teknologi.</p>
+                </div>
+            </div>
+            <div class="misi-card">
+                <div class="misi-number">05</div>
+                <div class="misi-content">
+                    <h4></h4>
+                    <p>Menumbuhkembangkan budaya sekolah sehat dan peduli lingkungan.</p>
+                </div>
+            </div>
+            <div class="misi-card">
+                <div class="misi-number">06</div>
+                <div class="misi-content">
+                    <h4></h4>
+                    <p>Melaksanakan pembelajaran dan penggunaan bahasa internasional.</p>
+                </div>
+            </div>
+            <div class="misi-card">
+                <div class="misi-number">07</div>
+                <div class="misi-content">
+                    <h4></h4>
+                    <p>Menerapkan pengelolaan sekolah yang mengacu pada standar manajemen mutu dengan melibatkan seluruh
+                        warga sekolah.</p>
+                </div>
+            </div>
+            <div class="misi-card">
+                <div class="misi-number">08</div>
+                <div class="misi-content">
+                    <h4></h4>
+                    <p>Menumbuhkembangkan prilaku religius dalam peserta didik sehingga dapat menghayati dan mengamalkan
+                        ajaran agama yang dianutnya dalam segala aspek kehidupan.</p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
 
-<?php 
-    // Include Footer
-    include 'includes/footer.php'; 
+<?php
+// Include Footer
+include 'includes/footer.php';
 ?>
